cf357b251611404f7d42970017993257739a3b04
sushantsc2014 <sushantsc2014@gmail.com>
2021-12-03 14:50:38 +0530

    first DAG

---
+++ My_first_DAG.py
@@ -0,0 +1,23 @@
+import airflow
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+args = {
+    'owner': 'airflow',
+    'reties': '2',
+    'retry_delay': timedelta(seconds=10)
+}
+
+dag= DAG(
+    dag_id='My_first_DAG',
+    catchup=False,
+    default_args=args,
+    max_active_runs=1,
+    schedule_interval=None,
+)
+
+
+Wnd = BashOperator(task_id="print_date",
+                    bash_command='echo "Hello Airflo"',
+                    dag=dag) 
